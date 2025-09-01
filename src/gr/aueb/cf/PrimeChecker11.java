e4f242723a0be228868ad14d5f75e6cc6de5846b
dimibald <jimmy_boixos@hotmail.com>
2025-09-01 14:34:11 +0300

    fix: prime checker.

--- src/gr/aueb/cf/PrimeChecker11.java
+++ src/gr/aueb/cf/PrimeChecker11.java
@@ -1,4 +1,34 @@
 package gr.aueb.cf;
 
 public class PrimeChecker11 {
+
+    public static void main(String[] args) {
+        int number = 17;
+
+        System.out.println("Αριθμός: " + number);
+        System.out.println("Είναι πρώτος; " + isPrime(number));
+
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        if (number <= 3) {
+            return true;
+        }
+
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        }
+
+        for (int i = 5; i * i <= number; i += 6) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
