aadef2de01f2b85e7b9393ec8cffbb38c7e4baaa
dimibald <jimmy_boixos@hotmail.com>
2025-09-01 12:27:12 +0300

    fix: score comparison method

--- src/gr/aueb/cf/ex3/Ex3.java
+++ src/gr/aueb/cf/ex3/Ex3.java
@@ -1,6 +1,6 @@
 package gr.aueb.cf.ex3;
 
-/*Δίνονται δύο πίνακες με 3 βαθμολογίες κάθε ένας. Συγκρίνετε τις βαθμολογίες και επιστρέψτε τους πόντους κάθε παίκτη
+/* Δίνονται δύο πίνακες με 3 βαθμολογίες κάθε ένας. Συγκρίνετε τις βαθμολογίες και επιστρέψτε τους πόντους κάθε παίκτη
 • Έστω οι Alice και Bob αξιολογούνται σε 3 κατηγορίες (π.χ., Μουσική, Προγραμματισμός, Γρήγορη Σκέψη). Κάθε κατηγορία
   δίνει βαθμολογία από 1 έως 100.
 • Αν η Alice έχει μεγαλύτερη βαθμολογία σε μια κατηγορία, παίρνει 1 πόντο. Αν ο Bob έχει μεγαλύτερη βαθμολογία,παίρνει
@@ -8,5 +8,43 @@ package gr.aueb.cf.ex3;
 • Επιστροφή: Πίνακας με 2 θέσεις [πόντοι_Alice, πόντοι_Bob]
 */
 
+import java.util.Arrays;
+
 public class Ex3 {
+
+    public static void main(String[] args) {
+
+        int[] aliceScores1 = {45, 99, 79} ;
+        int[] bobScores1 = {98 , 96, 44} ;
+
+        int[] result = compareScores(aliceScores1,bobScores1);
+        System.out.println("Alice : " + Arrays.toString(aliceScores1));
+        System.out.println("Bob : " + Arrays.toString(bobScores1));
+        System.out.println("Αποτελέσματα: [Alice]" + result[0] + ", Bob: " + result[1] + "]");
+
+
+
+
+
+    }
+
+    public static int[] compareScores(int[] aliceScores, int[] bobScores) {
+        if (aliceScores == null || bobScores == null ||
+            aliceScores.length != bobScores.length ) {
+            throw new IllegalArgumentException("Λάθος είσοδος");
+        }
+
+        int alicePoints = 0;
+        int bobPoints = 0;
+
+        for (int i = 0; i < aliceScores.length; i++) {
+            if (aliceScores[i] > bobScores[i]) {
+                alicePoints++;
+            }else if (bobScores[i] > aliceScores[i]) {
+                bobPoints++;
+            }
+        }
+
+        return new int[] {alicePoints,bobPoints};
+    }
 }
