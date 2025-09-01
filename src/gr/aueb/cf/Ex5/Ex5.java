6de432039bfacaf6746cd230edcde929a1f03fc9
dimibald <jimmy_boixos@hotmail.com>
2025-09-01 12:56:16 +0300

    fix: print Staircase

---
+++ src/gr/aueb/cf/Ex5/Ex5.java
@@ -0,0 +1,37 @@
+package gr.aueb.cf.Ex5;
+
+/*
+Δημιουργήστε μια σκάλα με # και κενά ( ), όπου: Το ύψος της σκάλας είναι ένας ακέραιος n.
+• Κάθε σκαλί έχει τον ίδιο αριθμό # με τον αριθμό της γραμμής του (πρώτη γραμμή: 1 #, δεύτερη γραμμή: 2 #, κ.ο.κ.).
+• Τα κενά ευθυγραμμίζονται στα δεξιά για να σχηματίσουν τη σκάλα.
+• Παράδειγμα για n = 4:
+#
+##
+###
+####
+ */
+public class Ex5 {
+
+    public static void main(String[] args) {
+        System.out.println("=== Σκάλα μεγέθους 4 ===");
+        printStaircase(4);
+        System.out.println();
+
+    }
+
+    public static void printStaircase(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Το n πρέπει να είναι θετικός ακέραιος");
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
